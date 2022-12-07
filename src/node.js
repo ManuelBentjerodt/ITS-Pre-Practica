c4852ca55bc8700f5c4a11201539edb753a52cb9
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2022-12-07 02:48:08 -0300

    feat(node model): adding bfs search

--- src/node.js
+++ src/node.js
@@ -1,49 +1,112 @@
 class Node {
-    constructor(_coordinate){
-
+    constructor(_coordinate, id=Date.now()){
+        this.id = id
         this.coordinate = _coordinate;  // array(x, y)
         this.parent = null;            // node
-        this.attached = {
-            childreanNodes: [],         // node
-            vinculo: null,              // type: 'deslizante', 'empotrado', etc
-            fuerzas: [],                // array(magnitud, angle)
-            momentos: []                // magnitud   
-        }
+        
+        this.childreanNodes = [],         // node
+        this.vinculo = null,              // type = 'deslizante', 'empotrado', etc
+        this.fuerzas = [],                // array(magnitud, angle)
+        this.momentos = []                // magnitud   
+        
+        this.konvaObject = null;
+    }
+
+    setId(id){
+        this.id = id
     }
 
     changeCoordinate(newCoordinate){
         this.coordinate = newCoordinate;
     }
 
-    putParent(parentNode){
+    setParent(parentNode){
         this.parent = parentNode;
     }
 
     addChild(node){
-        this.attached.childreanNodes.push(node);
+        this.childreanNodes.push(node);
     }
 
 
     addVinculo(vinculo){
         const types = new Set(["deslizante", "noDeslizante", "empotrado", "rotula", "biela"]);
         if (vinculo in types){
-            this.attached.vinculo.push(vinculo);
+            this.vinculo.push(vinculo);
         }
     }
 
     addFuerza(magnitud, angle){
-        this.attached.fuerzas.push((magnitud, angle));
+        this.fuerzas.push((magnitud, angle));
     }
 
     addMomento(type, magnitud){
         if (type === "positivo"){
-            this.attached.momentos.positivos.push(magnitud);
+            this.momentos.positivos.push(magnitud);
         } else if (type === "negativo"){
-            this.attached.momentos.negativos.push(magnitud);
+            this.momentos.negativos.push(magnitud);
         }
     }
 
+    setKonvaObject(object){
+        this.konvaObject = object;
+    }
+
+    findNodeById(_id, root=this){
+        const queue = [];
+        queue.push(root);
+
+        const discovered = [];
+        discovered.push(root);
+
+        while (queue.length){
+            let actual = queue.shift();
+            
+            if (actual.id == _id){
+                return actual;
+            }
+            actual.childreanNodes.forEach( child => {
+                if (!(child in discovered)){
+                    discovered.push(child)
+                    queue.push(child)
+                }
+            });
+        }
+
+        console.log("could not find element... returning null");
+        return null;
+
+    }
 }
 
 
+class Viga {
+    constructor(){
+        this.parents = [];
+    }
+
+    setParents(node1, node2){
+        this.parents = [node1, node2];
+    }
+}
+
+// function joinNodes(parent, child){
+//     parent.addChild(child);
+//     child.setParent(parent);
+// }
+
+// const n1 = new Node(null, 1)
+// const n2 = new Node(null, 2)
+// const n3 = new Node(null, 3)
+// const n4 = new Node(null, 4)
+
+// joinNodes(n1,n2)
+// joinNodes(n2,n3)
+// joinNodes(n3,n4)
+// joinNodes(n1,n3)
+
+
+
+// console.log(n1.findNodeById(0))
+// console.log(Date.now())
 
