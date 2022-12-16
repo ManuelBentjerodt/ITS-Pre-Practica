703f62edfd8b4c875c6e91f6eb6e0e82ff85b965
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2022-12-16 11:50:47 -0300

    save date in create task

---
+++ static/save_task.js
@@ -0,0 +1,7 @@
+function saveTask(){
+    const dclForm = document.querySelector("#id_dcl");
+    const dateForm = document.querySelector("#id_date");
+
+    dclForm.value = dcl.generateJSON()
+    dateForm.value = getDate()
+}
