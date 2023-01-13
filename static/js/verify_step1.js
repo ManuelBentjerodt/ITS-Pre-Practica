99eff5b39fd50a7f4d7f404de43349228b0eabe7
Dommingo Gonzalez <djgonzalez@miuandes.cl>
2023-01-13 19:18:39 -0300

    no se que esta mal del verufucar 1

--- static/js/verify_step1.js
+++ static/js/verify_step1.js
@@ -1,27 +1,32 @@
 const button = document.querySelector("#verifyButton")
 button.addEventListener("click", verifyTask);
-console.log("OLAAAAAA000");
-function verifyTask(e) {
-    const taskId = this.dataset.taskid;
-    console.log("dlc",dcl);
-    let dclJSON = dcl.generateCopy();
-    console.log(JSON.stringify(dclJSON));
+function verifyTask() {
+    // const taskId = this.dataset.taskid;
     console.log("dlc",dcl);
 
-    newDCL = recreateDcl(JSON.stringify(dclJSON));
+    let clone = Object.assign(Object.create(Object.getPrototypeOf(dcl)), dcl)
 
+    
+
+
+
+
+
+
+    let dclJSON = clone.generateCopy();
+
+    let newDCL = recreateDcl(JSON.stringify(dclJSON));
+    
     const standarizedDCL = standarizeDCL(newDCL);
     console.log("standarizedDCL:",standarizedDCL);
     console.log("correct dcl:",correctDcl);
     const standarizedCorrectDCL = standarizeDCL(correctDcl);
-
+    
     const areEqual = areDclEqual(standarizedDCL, standarizedCorrectDCL);
     console.log("areEqual:",areEqual);
-
-    console.log("dlc",dcl);
     
-    getBackDcl = recreateDcl(JSON.stringify(dclJSON));
-    console.log("getBackDcl",getBackDcl);
-    drawDCL(getBackDcl);
+    console.log("dcl",dcl);
+    
+    //drawDCL(newDCL);
    
 }   
