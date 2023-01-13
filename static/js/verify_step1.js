4c5fb3039b7453e586c76fdd2e6b4006e13e7357
Dommingo Gonzalez <djgonzalez@miuandes.cl>
2023-01-13 17:13:37 -0300

    bug de momento y fuerzas arreglado falta cositas

--- static/js/verify_step1.js
+++ static/js/verify_step1.js
@@ -1,36 +1,27 @@
-const button = document.querySelector("#saveButton")
-button.addEventListener("click", saveTask);
-
-function verifyTas(e) {
+const button = document.querySelector("#verifyButton")
+button.addEventListener("click", verifyTask);
+console.log("OLAAAAAA000");
+function verifyTask(e) {
     const taskId = this.dataset.taskid;
-    const dclJSON = dcl.generateCopy();
-    const sizeFactor = document.querySelector("#dim").value;
-    const difficulty = document.querySelector("#difficultyValue").innerText;
-    const statement = document.querySelector("#statement").value;
+    console.log("dlc",dcl);
+    let dclJSON = dcl.generateCopy();
+    console.log(JSON.stringify(dclJSON));
+    console.log("dlc",dcl);
 
-    const csfrToken = document.querySelector("[name=csrfmiddlewaretoken]").value;
-    const href = window.location.href;
+    newDCL = recreateDcl(JSON.stringify(dclJSON));
 
-    const data = JSON.stringify({
-        "difficulty": difficulty,
-        "sizeFactor": sizeFactor,
-        "statement": statement,
-        "dclJSON": dclJSON,
-    })
+    const standarizedDCL = standarizeDCL(newDCL);
+    console.log("standarizedDCL:",standarizedDCL);
+    console.log("correct dcl:",correctDcl);
+    const standarizedCorrectDCL = standarizeDCL(correctDcl);
+
+    const areEqual = areDclEqual(standarizedDCL, standarizedCorrectDCL);
+    console.log("areEqual:",areEqual);
+
+    console.log("dlc",dcl);
     
-    fetch(`${href}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRFToken": csfrToken,
-        },
-        body: data
-    })
-    .then((response) => {
-        return response.json()
-        
-    })
-    .then((data) => {
-        window.location.href = data.redirect
-    })
+    getBackDcl = recreateDcl(JSON.stringify(dclJSON));
+    console.log("getBackDcl",getBackDcl);
+    drawDCL(getBackDcl);
+   
 }   
